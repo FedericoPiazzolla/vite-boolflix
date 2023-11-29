693798f8cc97001e710202a9bee9a1e9f382e11d
FedericoPiazzolla <Federicop257@gmail.com>
2023-11-29 16:56:28 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<script >
+
+import axios from 'axios';
+import { store } from "./store"
+
+export default {
+  data() {
+    return {
+      store,
+    }
+  },
+  methods: {
+    getMovie() {
+      axios
+      .get(this.store.baseUrl + this.store.apiKey)
+      .then((resp) => {
+        console.log(resp.data);
+      });
+    }
+  }
+}
+</script>
+
+<template>
+
+</template>
+
+<style >
+
+</style>
---
+++ src/store.js
@@ -0,0 +1,6 @@
+import { reactive } from "vue";
+
+export const store = reactive({
+  baseUrl: "https://api.themoviedb.org/3/search/movie",
+  apiKey: "016927e2f9eb382388964ef9318383e9"
+});
