d808ad7f7fd73765665c12be42aa84f8fb525f6f
FedericoPiazzolla <Federicop257@gmail.com>
2023-11-29 18:55:30 +0100

    add: AppseriesCard

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,14 +1,15 @@
 <script>
 import { store } from '../store';
 import AppMovieCard from './AppMovieCard.vue';
+import AppSeriesCard from './AppSeriesCard.vue';
 
 export default {
     data() {
         return {
-            store,
+          store,
         };
     },
-    components: { AppMovieCard },
+    components: { AppMovieCard, AppSeriesCard },
 }
 </script>
 
@@ -31,7 +32,7 @@ export default {
       <h2>Series</h2>
       <div class="row">
         <div class="col" v-for="series in store.seriesList" :key="series.id">
-
+          <AppSeriesCard :series="series"/>
         </div>
       </div>
     </div>
---
+++ src/components/AppSeriesCard.vue
@@ -0,0 +1,25 @@
+<script>
+import { store } from '../store';
+
+export default{
+  data() {
+    return {
+      store,
+    }
+  },
+  props: {
+    series: Object,
+  }
+}
+</script>
+
+<template>
+  <ul>
+    <li>{{ series.name }}</li>
+    <li>{{ series.original_name }}</li>
+    <li>{{ series.original_language }}</li>
+    <li>{{ series.vote_average }}</li>
+  </ul>
+</template>
+
+<style></style>
