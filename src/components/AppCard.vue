d522de5ddea750a88b99f22a65c2837068ce2854
FedericoPiazzolla <Federicop257@gmail.com>
2023-11-30 16:42:49 +0100

    ref: card

--- src/components/AppCard.vue
+++ src/components/AppCard.vue
@@ -10,17 +10,38 @@ export default {
   },
   props: {
     movieObj: Object,
+  },
+  methods: {
+    getImgPath(lang) {
+      return new URL(`../assets/img/${lang}.png`, import.meta.url).href;
+    }
+  },
+  computed: {
+    isFlag() {
+      return this.flags.includes(this.movieObj.original_language);
+    },
+    title() {
+      return this.movieObj.title ? this.movieObj.title : this.movieObj.name;
+    },
+    originalTitle() {
+      return this.movieObj.original_title 
+        ? this.movieObj.original_title 
+        : this.movieObj.original_name;
+    }
   }
 }
 </script>
 
 <template>
   <ul>
-    <li>{{ movie.title }}</li>
-    <li>{{ movie.title }}</li>
-    <img src="" alt="">
-    <li>{{ movie.original_language }}</li>
-    <li>{{ movie.vote_average }}</li>
+    <li>{{ title }}</li>
+    <li>{{ originalTitle }}</li>
+    <img
+    v-if="isFlag"
+     :src="getImgPath(movieObj.original_language)" 
+     alt="">
+    <li v-else>{{ movieObj.original_language }}</li>
+    <li>{{ movieObj.vote_average }}</li>
   </ul>
 </template>
 
